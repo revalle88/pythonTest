3bf46aa8c194f26038cdecedf3542b09f9bce2c7
revalle88 <revalle88@gmail.com>
2017-08-31 23:09:15 +0300

    django admin

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Person
+from .models import Department
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Person)
