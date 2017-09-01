de6ad721531a408dd56a6ad4f0d9a025593400ce
revalle88 <revalle88@gmail.com>
2017-09-01 22:51:07 +0300

    employees list + pagination

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^employees/$', views.employees, name='employees'),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Person
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Test Application")
+
+def employees(request):
+	person_list = Person.objects.all()
+	paginator = Paginator(person_list, 3)
+	page = request.GET.get('page')
+	try:
+	    persons = paginator.page(page)
+	except PageNotAnInteger:
+	    persons = paginator.page(1)
+	except EmptyPage:
+	    persons = paginator.page(paginator.num_pages)
+	return render(request, 'employees.html', {'person_list': persons})
