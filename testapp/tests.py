301f94bba832d12f5a1bb250b058600d2a3d85d7
revalle88 <revalle88@gmail.com>
2017-09-05 22:16:05 +0300

    add alpha test

--- testapp/tests.py
+++ testapp/tests.py
@@ -33,6 +33,15 @@ class ModelsTest(TestCase):
 class AlphaTest(TestCase):
 	def setUp(self):
 		self.client = Client()
-	def test_alphaView
+	def test_alphaView(self):
 		response = self.client.get('/testapp/alpha/')
 		self.assertEqual(response.status_code, 200)
+	def test_alphaFilter(self):
+		now = datetime.datetime.now()
+		department = Department.objects.create(name='foo', creation_date=now)
+		self.assertTrue(Person.objects.create(name='foo', sirname='bar', patronymic='foobar', birthdate=now, email='foo@bar.ru', employDate=now, occupation = 'occ', department=department))
+		self.assertTrue(Person.objects.create(name='goo', sirname='gar', patronymic='foobar', birthdate=now, email='foo@bar.ru', employDate=now, occupation = 'occ', department=department))
+
+		response = self.client.get('/testapp/alpha/')
+		self.assertContains( response, '<input name = "letters" type = "hidden" value = "a-b" />', status_code=200 )
+		self.assertContains( response, '<input name = "letters" type = "hidden" value = "c-g" />', status_code=200 )
