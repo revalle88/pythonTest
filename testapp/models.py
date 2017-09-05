4cdffc2327f207687b107d8ab87ca95c5d1d4363
revalle88 <revalle88@gmail.com>
2017-09-05 21:26:13 +0300

    model unicode methods for displaying objects

--- testapp/models.py
+++ testapp/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Department(models.Model):
 	name = models.CharField(max_length=100)
 	creation_date = models.DateTimeField('date created')
+	
+	def __unicode__(self):
+		return u'%s' % (self.name)
+
 
 class Person(models.Model):
 	department = models.ForeignKey(Department, on_delete=models.CASCADE)
@@ -16,3 +20,7 @@ class Person(models.Model):
 	employDate = models.DateField('employment date')
 	retireDate = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
 	occupation = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return u'%s %s' % (self.name, self.sirname)
+
