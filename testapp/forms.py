016b8d0b48e5a0f162f9efbc7af653d79f162032
revalle88 <revalle88@gmail.com>
2017-09-05 21:34:05 +0300

    filter form required false

--- testapp/forms.py
+++ testapp/forms.py
@@ -4,5 +4,5 @@ from django.contrib.admin.widgets import AdminDateWidget
 
 class DepartmentForm(forms.Form):
 	departments = Department.objects.all()
-	department = forms.ModelChoiceField(queryset = departments)
-	retire = forms.DateField(widget = AdminDateWidget())
+	department = forms.ModelChoiceField(queryset = departments, required = False)
+	retire = forms.DateField(widget = AdminDateWidget(),  required = False)
